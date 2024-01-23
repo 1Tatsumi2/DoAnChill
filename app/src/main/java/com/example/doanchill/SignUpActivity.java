7ac888dc7e84a5fb8266a21a2fb594bd0bab98b5
1Tatsumi2 <tranthuong1212@gmail.com>
2024-01-23 09:59:11 +0700

    THUONG GA

--- app/src/main/java/com/example/doanchill/SignUpActivity.java
+++ app/src/main/java/com/example/doanchill/SignUpActivity.java
@@ -1,5 +1,7 @@
 package com.example.doanchill;
 
+import static androidx.constraintlayout.helper.widget.MotionEffect.TAG;
+
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,6 +10,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -19,6 +22,7 @@ import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class SignUpActivity extends AppCompatActivity {
 
@@ -26,7 +30,7 @@ public class SignUpActivity extends AppCompatActivity {
     Button signup;
     TextView signin;
     ProgressBar progressBar;
-    private FirebaseAuth mAuth;
+    FirebaseAuth mAuth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -45,15 +49,44 @@ public class SignUpActivity extends AppCompatActivity {
         signup.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String username = Username.getText().toString();
-                String email = Email.getText().toString();
-                String password = Password.getText().toString();
-                String confirmpass = Confirmpass.getText().toString();
                 progressBar.setVisibility(View.VISIBLE);
-                signUpWithFirebase();
-                signup.setEnabled(false);
-                signup.setTextColor(getResources().getColor(R.color.white));
+                String user = Username.getText().toString();
+                String email = Email.getText().toString().trim();
+                String password = Password.getText().toString().trim();
+                String confirm = Confirmpass.getText().toString().trim();
 
+                if (user.isEmpty()) {
+                    Username.setError("Username cannot be empty");
+                }
+                if (email.isEmpty()) {
+                    Email.setError("Email cannot be empty");
+                }
+                if (password.isEmpty()) {
+                    Password.setError("Password cannot be empty");
+                }
+                if (confirm.isEmpty()) {
+                    Confirmpass.setError("Confirm pass cannot be empty");
+                } else {
+
+                                mAuth.createUserWithEmailAndPassword(user, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                                    @Override
+                                    public void onComplete(@NonNull Task<AuthResult> task) {
+                                        if (task.isSuccessful()) {
+                                            Toast.makeText(SignUpActivity.this, "SignUp Successful", Toast.LENGTH_SHORT).show();
+                                            startActivity(new Intent(SignUpActivity.this, SignInActivity.class));
+                                        } else {
+                                            Toast.makeText(SignUpActivity.this, "SignUp Failed" + task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                                        }
+                                    }
+                                });
+                            }
+                        }
+
+
+
+
+
+        });
         signin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -62,139 +95,6 @@ public class SignUpActivity extends AppCompatActivity {
                 finish();
             }
         });
-        Username.addTextChangedListener(new TextWatcher() {
-            @Override
-            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
-
-            }
-
-            @Override
-            public void onTextChanged(CharSequence s, int start, int before, int count) {
-                CheckInputs();
-            }
-
-            @Override
-            public void afterTextChanged(Editable s) {
-
-            }
-        });
-        Email.addTextChangedListener(new TextWatcher() {
-            @Override
-            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
-
-            }
-
-            @Override
-            public void onTextChanged(CharSequence s, int start, int before, int count) {
-                CheckInputs();
-            }
-
-            @Override
-            public void afterTextChanged(Editable s) {
-
-            }
-        });
-        Password.addTextChangedListener(new TextWatcher() {
-            @Override
-            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
-
-            }
-
-            @Override
-            public void onTextChanged(CharSequence s, int start, int before, int count) {
-                CheckInputs();
-            }
-
-            @Override
-            public void afterTextChanged(Editable s) {
-
-            }
-        });
-        Confirmpass.addTextChangedListener(new TextWatcher() {
-            @Override
-            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
-
-            }
-
-            @Override
-            public void onTextChanged(CharSequence s, int start, int before, int count) {
-
-            }
-
-            @Override
-            public void afterTextChanged(Editable s) {
-                CheckInputs();
-            }
-        });
-
     }
 
-    private void signUpWithFirebase()
-    {
-        if(Email.getText().toString().matches("[a-zA-z0-9._-]+@[a-z]+\\.+[a-z]+"))
-        {
-            if(Password.getText().toString().equals(Confirmpass.getText().toString()))
-            {
-                mAuth.createUserWithEmailAndPassword(Email.getText().toString(), Password.getText().toString())
-                        .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
-                            @Override
-                            public void onComplete(@NonNull Task<AuthResult> task) {
-                                if(task.isSuccessful())
-                                {
-                                    Intent intent = new Intent(SignUpActivity.this,MainActivity.class);
-                                    startActivity(intent);
-                                    finish();
-                                } else{
-                                    Toast.makeText(SignUpActivity.this, "Authentication failed.", Toast.LENGTH_SHORT).show();
-                                }
-                            }
-                        });
-            } else {
-                Confirmpass.setError("Password doesn't match.");
-                signup.setEnabled(true);
-                signup.setTextColor(getResources().getColor(R.color.white));
-            }
-        } else {
-            Email.setError("Invalid Email Pattern!");
-            signup.setEnabled(true);
-            signup.setTextColor(getResources().getColor(R.color.white));
-        }
-    }
-
-    private void CheckInputs()
-    {
-        if(!Username.getText().toString().isEmpty())
-        {
-            if(!Email.getText().toString().isEmpty())
-            {
-                if(!Password.getText().toString().isEmpty() && Password.length() >= 6)
-                {
-                    if(!Confirmpass.getText().toString().isEmpty())
-                    {
-                        signup.setEnabled(true);
-                        signup.setTextColor(getResources().getColor(R.color.white));
-                    }
-                    else {
-                        signup.setEnabled(false);
-                        signup.setTextColor(getResources().getColor(R.color.white));
-                    }
-                }
-                else {
-                    signup.setEnabled(false);
-                    signup.setTextColor(getResources().getColor(R.color.white));
-                }
-            }
-            else {
-                signup.setEnabled(false);
-                signup.setTextColor(getResources().getColor(R.color.white));
-            }
-        }
-        else {
-            signup.setEnabled(false);
-            signup.setTextColor(getResources().getColor(R.color.white));
-        }
-
-    }
-        });
-    }
 }
