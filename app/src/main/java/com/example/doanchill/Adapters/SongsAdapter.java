bbc57e0723b126e782314e4a8cddc6ccaa5f64c7
codeTodies <lethanhsang2004.lts@gmail.com>
2024-01-31 08:47:46 +0700

    demo 1

---
+++ app/src/main/java/com/example/doanchill/Adapters/SongsAdapter.java
@@ -0,0 +1,45 @@
+package com.example.doanchill.Adapters;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import com.example.doanchill.Class.Song;
+import com.example.doanchill.R;
+
+import java.util.List;
+
+public class SongsAdapter extends ArrayAdapter<Song> {
+
+    public SongsAdapter(@NonNull Context context, @NonNull List<Song> objects) {
+        super(context, 0, objects);
+    }
+
+    @NonNull
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+        convertView = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_song, null);
+
+        TextView tvTitle = convertView.findViewById(R.id.tvTitle);
+        TextView tvArtist = convertView.findViewById(R.id.tvArtist);
+        TextView tvDuration=convertView.findViewById(R.id.tvDuration);
+        Song song = getItem(position);
+        tvTitle.setText(song.getTitle());
+        tvArtist.setText(song.getArtist());
+        tvDuration.setText(millisecondsToString(song.getDuration()));
+
+
+        return convertView;
+    }
+    public String millisecondsToString(int time) {
+        int minutes = time / 1000 / 60;
+        int seconds = time / 1000 % 60;
+        return minutes + ":" + (seconds < 10 ? "0" : "") + seconds;
+    }
+}
---
+++ app/src/main/java/com/example/doanchill/Interface/ActionPlaying.java
@@ -0,0 +1,7 @@
+package com.example.doanchill.Interface;
+
+public interface ActionPlaying {
+    void nextClicked();
+    void prevClicked();
+    void playClicked();
+}
